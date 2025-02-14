06d390756f2ec82c9a9bac620bc817fdb72fc3d6
ren <janren.delasalas@gmail.com>
2025-02-15 01:00:52 +0800

    redepley with no prerender

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,6 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const dev = process.argv.includes('dev');
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -14,9 +13,8 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({ fallback: 'index.html' }),
 		paths: {
-			base: dev ? '' : '/BlogSvelte-Supabase'
-		},
-		prerender: { entries: [], concurrency: 5 }
+			base: ''
+		}
 	}
 };
 
