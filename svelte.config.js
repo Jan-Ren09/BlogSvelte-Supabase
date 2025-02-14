7d30e8ea6a683bd475219b32d5d4c66cf16ee73a
ren <janren.delasalas@gmail.com>
2025-02-14 23:51:42 +0800

    base dev

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.argv.includes('dev');
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -13,7 +14,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({ fallback: 'index.html' }),
 		paths: {
-			base: 'production' ? '/BlogSvelte-Supabase' : ''
+			base: dev ? '/BlogSvelte-Supabase' : ''
 		},
 		prerender: { entries: [] }
 	}
