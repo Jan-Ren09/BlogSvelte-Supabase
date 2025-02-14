d8c95fd3c7acb34c970adbc9abc05f963010b0a1
ren <janren.delasalas@gmail.com>
2025-02-14 23:41:49 +0800

    paths production

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({ fallback: 'index.html' }),
 		paths: {
-			base: dev ? '/BlogSvelte-Supabase' : ''
+			base: 'production' ? '/BlogSvelte-Supabase' : ''
 		},
 		prerender: { entries: [] }
 	}
